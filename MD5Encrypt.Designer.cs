1dfe0927f8585214b5001e34aae645c678941cd8
unknown <choushuimatong123@126.com>
2019-05-29 22:01:25 +0800

    基础版本。

---
+++ MD5Encrypt.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace MD5FileCreator
+{
+    partial class MD5Encrypt
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MD5Encrypt));
+            this.tbMD5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbMD5
+            // 
+            this.tbMD5.BackColor = System.Drawing.Color.White;
+            this.tbMD5.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tbMD5.Location = new System.Drawing.Point(76, 88);
+            this.tbMD5.Multiline = true;
+            this.tbMD5.Name = "tbMD5";
+            this.tbMD5.ReadOnly = true;
+            this.tbMD5.Size = new System.Drawing.Size(629, 111);
+            this.tbMD5.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(22, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "md5值";
+            // 
+            // tbPath
+            // 
+            this.tbPath.BackColor = System.Drawing.Color.White;
+            this.tbPath.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tbPath.Location = new System.Drawing.Point(76, 56);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(591, 26);
+            this.tbPath.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(7, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 14);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "文件路径";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(256, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 39);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "MD5加密程序";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(495, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 14);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Create By @MaTongGe On Github";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(230, 205);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 33);
+            this.btnOK.TabIndex = 6;
+            this.btnOK.Text = "加密";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(428, 205);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 33);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "清空";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("宋体", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnBrowse.Location = new System.Drawing.Point(673, 56);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(31, 26);
+            this.btnBrowse.TabIndex = 8;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // MD5Encrypt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 248);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbMD5);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MD5Encrypt";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MD5加密程序";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbMD5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnBrowse;
+    }
+}
+
---
+++ MD5Encrypt.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MD5FileCreator
+{
+    public partial class MD5Encrypt : Form
+    {
+        public MD5Encrypt()
+        {
+            InitializeComponent();
+        }
+
+        public static string EncryptWithMD5(string source)
+        {
+            byte[] sor = Encoding.UTF8.GetBytes(source);
+            MD5 md5 = MD5.Create();
+            byte[] result = md5.ComputeHash(sor);
+            StringBuilder strbul = new StringBuilder(40);
+            for (int i = 0; i < result.Length; i++)
+            {
+                //加密结果"x2"结果为32位,"x3"结果为48位,"x4"结果为64位
+                strbul.Append(result[i].ToString("x4"));
+            }
+            return strbul.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            tbMD5.Text = "";
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+            if (tbPath.Text == "" || !File.Exists(tbPath.Text))
+            {
+                MessageBox.Show("文件地址不合法或文件不存在！", "MD5加密程序", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            tbMD5.Text = EncryptWithMD5(tbPath.Text);
+        }
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "请选择要加密的文件";
+            dialog.Filter = "所有文件类型|*.*";
+            
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = dialog.FileName;
+                MessageBox.Show("加密成功！", "MD5加密程序", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
